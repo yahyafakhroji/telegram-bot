862114a01be63f2be467b988bce02d7b31d89398
Yahya Fakhroji <yahya.fakhroji@gmail.com>
2024-03-01 23:34:48 +0700

    Implement Product Data

--- src/modules/tg/commands/start.command.ts
+++ src/modules/tg/commands/start.command.ts
@@ -8,29 +8,40 @@ export class StartCommand extends AbstractCommand {
     super(bot);
   }
 
-  handler(): void {
+  async getProducts() {
+    const res = await fetch('https://api.muggle.link/api/products/283a8736-004e-4b72-a871-149dc8303576');
+    const { data } = await res.json();
+    return data.product;
+  }
+
+  async handler() {
+    const product = await this.getProducts();
+    
+      
+    let message = '<b>Thank you for your order!</b>\n\n';
+    message +=
+      'This message confirms your order with <b>Molla, inc</b>. Your order number is <a href="https://www.google.com/search?q=12345">#12345</a>.\n\n';
+    message += '📦 <b>Product</b>\n\n';
+    message += '<blockquote>';
+    message += `<u>${product.name}</u>\n\n`;
+    message += `${product.description}\n\n`;
+    message += `🏷️ <b>${product.currency} ${product.price}</b>`;
+    message += '</blockquote>\n\n';
+    message += '🧾 <b>Order Details</b>\n\n';
+    message +=
+      '<b> - Order Date:</b> <i>' +
+      dayjs().format('MMMM DD, YYYY HH:mm') +
+      '</i>\n';
+    message +=
+      '<b> - Shipping Address:</b> <i>Malang Regency, East Java, Indonesia</i>\n';
+    message += `<b> - Grand Total:</b> <i>${product.currency} ${product.price}</i>\n`;
+    message += '<b> - Payment Method:</b> <i>Muggle Link</i>\n\n';
+    message += 'Make payment by click <b>Pay Now</b> button.';
     this.bot.start(async (ctx) => {
-      let message = '<b>Thank you for your order!</b>\n\n';
-      message +=
-        'This message confirms your order with <b>Molla, inc</b>. Your order number is <a href="https://www.google.com/search?q=12345">#12345</a>.\n\n';
-      message += '📦 <b>Products</b>\n\n';
-      message += '<blockquote>';
-      message += "- 2 x Air Jordan 1 Low Women's Shoes = USD 200\n";
-      message += "- 1 x Air Jordan 1 Low Men's Shoes = USD 150\n";
-      message += '</blockquote>\n\n';
-      message += '🧾 <b>Order Details</b>\n\n';
-      message +=
-        '<b> - Order Date:</b> <i>' +
-        dayjs().format('MMMM DD, YYYY HH:mm') +
-        '</i>\n';
-      message +=
-        '<b> - Shipping Address:</b> <i>Malang Regency, East Java, Indonesia</i>\n';
-      message += '<b> - Grand Total:</b> <i>USD 350</i>\n';
-      message += '<b> - Payment Method:</b> <i>Muggle Link</i>\n\n';
-      message += 'Make payment by click <b>Pay Now</b> button.';
+      
 
       ctx.replyWithPhoto(
-        { url: 'https://i.ibb.co/s1PYs1v/air-jordan.jpg' },
+        { url: product.image_url },
         {
           caption: message,
           parse_mode: 'HTML',
